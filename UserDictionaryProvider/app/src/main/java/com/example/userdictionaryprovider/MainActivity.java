e0be36f23291035cd920944a2a82b609fed2ab1f
Aarthy <aarthyr.2020@kgkite.ac.in>
2022-03-29 08:12:00 -0700

    Initial Commit

---
+++ UserDictionaryProvider/app/src/main/java/com/example/userdictionaryprovider/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.userdictionaryprovider;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ContentResolver resolver = getContentResolver();
+        Uri uri = ContentHelper.insertContent(resolver);
+        Log.d(TAG, "Uri:"+uri.getPath());
+        String data = ContentHelper.fetchContent(resolver, uri);
+        TextView view = (TextView)findViewById(R.id.data);
+        view.setText(data);
+
+        int rows = ContentHelper.updateContent(resolver, uri);
+        Log.d(TAG, "Rows Updated:"+ rows);
+        data = ContentHelper.fetchContent(resolver, uri);
+        TextView updateView = (TextView)findViewById(R.id.updatedata);
+        updateView.setText(data);
+
+        rows = ContentHelper.deleteContent(resolver);
+        Log.d(TAG, "Rows Deleted:"+ rows);
+    }
+}
